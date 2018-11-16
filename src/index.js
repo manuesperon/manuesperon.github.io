6c0812d7a90127b94ce8e12d8ee0a6dc65868ed8
manuesperon <manuelesperon23@gmail.com>
2018-11-16 13:41:46 +1300

    Disable service worker and fix css bug in production

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
