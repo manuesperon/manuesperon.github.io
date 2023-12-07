cc9de851f7426f0f1db8245bdafdd388263b375d
Manuel Esperon <hello@manuesperon.com>
2023-12-07 16:08:23 +1300

    Add mail icon and add title property to all icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import Image from 'next/image';
 
 import GithubIcon from '@components/icons/GithubIcon';
 import LinkedInIcon from '@components/icons/LinkedInIcon';
+import MailIcon from '@components/icons/MailIcon';
 
 const Home = () => {
   const iconStyles = 'fill-white hover:fill-aquaGreen transition-colors duration-300';
+
   return (
     <div className="flex flex-col items-center gap-8 p-8 text-center">
       <Image className="rounded-full" src="/images/memoji.jpeg" alt="Photo placeholder" width={300} height={300} />
@@ -21,11 +23,14 @@ const Home = () => {
         <Button>Get to know more about me!</Button>
       </Link> */}
       <div className="flex gap-4">
-        <a href="https://github.com/manuesperon/manuesperon.github.io" target="_blank">
+        <a href="https://github.com/manuesperon/manuesperon.github.io" target="_blank" title="Github">
           <GithubIcon width={40} height={40} className={iconStyles} />
         </a>
-        <a href="https://www.linkedin.com/in/manuel-esperon-b9b301145/" target="_blank">
+        <a href="https://www.linkedin.com/in/manuel-esperon-b9b301145/" target="_blank" title="LinkedIn">
           <LinkedInIcon width={40} height={40} className={iconStyles} />
+        </a>
+        <a href="mailto:hello@manuesperon.com" title="Email me!">
+          <MailIcon width={40} height={40} className={iconStyles} />
         </a>
       </div>
     </div>
