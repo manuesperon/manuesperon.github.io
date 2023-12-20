302708b8e4107f2de3ec592c66601b2d046ded4c
Manuel Esperon <hello@manuesperon.com>
2023-12-20 15:07:18 +1300

    Add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,51 @@
+import BlenderIcon from '@components/icons/BlenderIcon';
+import GraphQLIcon from '@components/icons/GraphQLIcon';
+import NextIcon from '@components/icons/NextIcon';
+import NodeIcon from '@components/icons/NodeIcon';
+import ReactIcon from '@components/icons/ReactIcon';
+import ThreeIcon from '@components/icons/ThreeIcon';
+import TypescriptIcon from '@components/icons/TypescriptIcon';
+import Link from 'next/link';
+
+const TechnologiesContainer = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex items-center sm:gap-6 gap-3 border-2 border-aquaGreen rounded-full p-4">{children}</div>;
+};
+
+const AboutPage = () => {
+  const paragraphStyles = 'lg:text-xl text-md  my-8 text-center';
+  const iconStyles =
+    'lg:w-[80px] lg:h-[80px] md:w-[50px] md:h-[50px] w-[35px] h-[35px] fill-white hover:fill-aquaGreen hover:text-aquaGreen transition-colors duration-300';
+
+  return (
+    <div className="max-w-4xl flex flex-col items-center justify-around">
+      <h1>About me</h1>
+      <p className={paragraphStyles}>
+        I&apos;m a full-stack engineer with a passion for <span className="text-aquaGreen">front-end</span> development.
+        I like blending tech skills with <span className="text-aquaGreen">creativity</span> to craft beautiful
+        user-friendly interfaces.&nbsp;
+        <Link
+          href="/#contact"
+          className="underline decoration-aquaGreen underline-offset-[5px] hover:text-aquaGreen transition-colors duration-300"
+        >
+          Let&apos;s bring your ideas to life!
+        </Link>
+        &nbsp; 💪
+      </p>
+      <p className={paragraphStyles}>These are some of the tools that have helped me bring my projects to life</p>
+      <TechnologiesContainer>
+        <ReactIcon className={iconStyles} />
+        <TypescriptIcon className={`${iconStyles}`} />
+        <GraphQLIcon className={iconStyles} />
+        <NextIcon className={iconStyles} />
+        <NodeIcon className={iconStyles} />
+      </TechnologiesContainer>
+      <p className={paragraphStyles}>...and these are the ones I&apos;m currently experimenting with!</p>
+      <TechnologiesContainer>
+        <ThreeIcon className={iconStyles} />
+        <BlenderIcon className={`${iconStyles}`} />
+      </TechnologiesContainer>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/components/icons/ReactIcon.tsx
@@ -0,0 +1,22 @@
+import { IconProps } from '@types';
+
+const ReactIcon = ({ width, height, className }: IconProps) => {
+  return (
+    <svg
+      className={className}
+      height={height}
+      width={width}
+      viewBox="-10.5 -9.45 21 18.9"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="0" cy="0" r="2" fill="currentColor"></circle>
+      <g stroke="currentColor" strokeWidth="1" fill="none">
+        <ellipse rx="10" ry="4.5"></ellipse>
+        <ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse>
+        <ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse>
+      </g>
+    </svg>
+  );
+};
+
+export default ReactIcon;
