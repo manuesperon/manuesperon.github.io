71a824cd8924ac94de29f7ddf1068afd66c9b3bf
Manuel Esperon <hello@manuesperon.com>
2023-12-27 17:17:40 +1300

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
+import GoogleAnalytics from '@components/GoogleAnalytics';
 
 export const metadata: Metadata = {
   title: 'Manuel Esperon',
@@ -9,9 +10,11 @@ export const metadata: Metadata = {
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ID;
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col ">
+        {GA_TRACKING_ID && <GoogleAnalytics gaID={GA_TRACKING_ID} />}
         <Header />
         <main className="mx-auto mt-6 md:p-8 p-4">{children}</main>
         <Footer />
